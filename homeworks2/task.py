eb0521bd1bdae926d50f864f24cd40a89b7f31bb
Vadim Bogdan <bogdanvadim2002@gmail.com>
2022-11-23 00:55:11 +0300

    Fixed after flask8 review

--- homeworks2/task.py
+++ homeworks2/task.py
@@ -1,4 +1,4 @@
-#рассчитать расстояние между двумя точками на плоскости
+# рассчитать расстояние между двумя точками на плоскости
 import math
 
 coordinate_x1 = int(input("Введите координату x1: "))
@@ -6,5 +6,6 @@ coordinate_y1 = int(input("Введите координату y1: "))
 coordinate_x2 = int(input("Введите координату x2: "))
 coordinate_y2 = int(input("Введите координату y2: "))
 
-distance_between_points = math.sqrt((pow(coordinate_x2 - coordinate_x1,2)) + pow(coordinate_y2 - coordinate_y1,2))
-print("Расстояние между точками: ",round(distance_between_points,2))
+distance_between_points = math.sqrt((pow(coordinate_x2 - coordinate_x1, 2)) +
+                                    (pow(coordinate_y2 - coordinate_y1, 2)))
+print("Расстояние между точками: ", round(distance_between_points, 2))
