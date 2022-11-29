8faa7e7b688fda3e4b2506e56c897311fba2437d
Vadim Bogdan <bogdanvadim2002@gmail.com>
2022-11-29 22:23:27 +0300

    first task

--- homeworks3/task_two.py
+++
@@ -1,7 +0,0 @@
-# вывести строку в обратной порядке
-str_two_words = input("Введите строку состоящую из двух слов: ")
-
-list_str = str_two_words.split()
-list_str.reverse()
-result_str = " ".join(list_str)
-print(result_str)
