9e88782e8eccde21f135d92feddc45aa4d2ec002
Vadim Bogdan <bogdanvadim2002@gmail.com>
2022-12-04 04:10:55 +0300

    added with open as

--- homeworks5/task_three.py
+++ homeworks5/task_three.py
@@ -1,7 +1,7 @@
-f = open('file_1.txt', 'r')
-letter = input("Введите букву для нахождения числа её вхождений в файл:")
-text = f.read()
-new_text = text.casefold()
-qauntity = new_text.count(letter)
+with open('file_1.txt', 'r') as f:
+    letter = input("Введите букву для нахождения числа её вхождений в файл:")
+    text = f.read()
+    new_text = text.casefold()
+    qauntity = new_text.count(letter)
 
-print(qauntity)
+    print(qauntity)
