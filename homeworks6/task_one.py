62f71a7366fffabd67dd11644589e2c20e2212a8
Vadim Bogdan <bogdanvadim2002@gmail.com>
2022-12-11 22:35:06 +0300

    dz 6 after flake8

--- homeworks6/task_one.py
+++ homeworks6/task_one.py
@@ -1,5 +1,5 @@
 def dist(*args):
-    distance_sum = 0 
+    distance_sum = 0
     for index, coords in enumerate(args):
         if index == len(args) - 1:
             break
@@ -8,8 +8,7 @@ def dist(*args):
         length = ((x2-x1)**2 + (y2-y1)**2)**0.5
         distance_sum += length
     return distance_sum
-        
-result = dist((1,1), (1,2), (2,2), (3,3))
+
+
+result = dist((1, 1), (1, 2), (2, 2), (3, 3))
 print(result)
-
-
